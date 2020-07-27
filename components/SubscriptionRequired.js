32721304b787884abd3567b190ffbfed984103cb
angel <60677827+angelabubo@users.noreply.github.com>
2020-07-27 18:37:43 -0400

    Added new component to display non premium message

---
+++ components/SubscriptionRequired.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+    backgroundColor: "#a8dadc",
+    letterSpacing: "3px",
+  },
+  title: {
+    letterSpacing: "2px",
+  },
+  subtitle: {
+    letterSpacing: "1.5px",
+  },
+});
+
+const SubscriptionRequired = ({ feature }) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root} variant="outlined" elevation={3}>
+      <CardContent>
+        <Typography variant="h2" component="h2" className={classes.title}>
+          Ooops!
+        </Typography>
+        <Typography
+          variant="h6"
+          component="h2"
+          color="textSecondary"
+          className={classes.subtitle}
+        >
+          You must be subscribed to WarrenAi Premium to view {feature}.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button href="/account/pricingplans" size="medium">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default SubscriptionRequired;
